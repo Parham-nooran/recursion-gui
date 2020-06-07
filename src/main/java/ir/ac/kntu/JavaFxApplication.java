5ba8bb154861820f78988e69bc6c51ed56647e61
Parham <parhamnooranbakht@gmail.com>
2020-06-07 15:34:06 +0430

    basic picture formed and completed

--- src/main/java/ir/ac/kntu/JavaFxApplication.java
+++ src/main/java/ir/ac/kntu/JavaFxApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -15,17 +16,16 @@ public class JavaFxApplication extends Application {
 
     public void start(Stage stage) throws Exception {
         Pane root = new Pane();
-        root.setStyle("-fx-border-width: 0 0 5 0; -fx-border-style: dotted;");
-        Scene scene = new Scene(root, 800, 600, Color.rgb(240, 240, 240));
-        
-        //TODO your code goes here
-        // try to seprate logic from GUI 
-
-
-        // Setting stage properties
+        //root.setStyle("-fx-border-width: 0 0 5 0; -fx-border-style: dotted;");
+        Scene scene = new Scene(root, 610, 610);
+        // try to separate logic from GUI
+        RecursiveGUI recursiveGUI = new RecursiveGUI();
+        Circle circle = new Circle(300, 300, 300, Color.GOLD);
+        root.getChildren().add(circle);
+        double radius = 300;
+        recursiveGUI.draw(root, radius, radius, radius/4);
         stage.initStyle(StageStyle.UTILITY);
         stage.setTitle("Recursion in JavaFX!");
-        
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/ir/ac/kntu/RecursiveGUI.java
+++ src/main/java/ir/ac/kntu/RecursiveGUI.java
@@ -1,5 +1,36 @@
 package ir.ac.kntu;
 
-public class RecursiveGUI{
-    
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
+
+
+public class RecursiveGUI {
+    public void draw(Pane pane, double x,double y,double radius){
+        Circle center = new Circle(x, y, 2*radius, Color.BLACK);
+        Circle circle0 = new Circle(x+3*radius, y, radius, Color.RED);
+        Circle circle45 = new Circle(x+2.121*radius, y-2.121*radius, radius, Color.CHARTREUSE);
+        Circle circle90 = new Circle(x, y-3*radius, radius, Color.YELLOW);
+        Circle circle135 = new Circle(x-2.121*radius, y-2.121*radius, radius, Color.ROYALBLUE);
+        Circle circle180 = new Circle(x-3*radius, y, radius, Color.MEDIUMPURPLE);
+        Circle circle245 = new Circle(x-2.121*radius, y+2.121*radius, radius, Color.	GREENYELLOW);
+        Circle circle270 = new Circle(x, y+3*radius, radius, Color.	FUCHSIA);
+        Circle circle345 = new Circle(x+2.121*radius, y+2.121*radius, radius, Color.BLUE);
+        pane.getChildren().addAll(center, circle0, circle45, circle90, circle135,
+                circle180, circle245,circle270, circle345);
+        if(radius>4) {
+            draw(pane, x, y, radius/2);
+            draw(pane, x, y-3*radius, radius/4);
+            draw(pane, x-3*radius, y, radius/4);
+            draw(pane, x, y+3*radius, radius/4);
+            draw(pane, x+3*radius, y, radius/4);
+            draw(pane, x+2.121*radius, y+2.121*radius, radius/4);
+            draw(pane, x-2.121*radius, y-2.121*radius, radius/4);
+            draw(pane, x+2.121*radius, y-2.121*radius, radius/4);
+            draw(pane, x-2.121*radius, y+2.121*radius, radius/4);
+        }
+    }
 }
